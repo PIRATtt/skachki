ee7f62a5a7a2187e9742fccac8abc8292db7d9ca
root <ilya.crisanov@yandex.ru>
2023-04-04 00:47:48 +0300

    }{}

--- analys.py
+++ analys.py
@@ -1,14 +1,13 @@
 import os
 import json
-pylint = os.popen("""
-sudo pylint app.py  --const-rgx='[a-z\_][a-z0-9\_]{2,30}$'
-""").read()
-result_code=float(pylint.split('\n')[-3].split()[6].split('/')[0])
-if result_code < 5.5:
-    print("Error code!")
+pylint = os.popen('pylint app.py').read()
+res_code = float(pylint.split('\n')[-3].split()[6].split('/')[0])
 
-os.system('sudo bandit -iii -lll -q -r ./skachki/ -o bandit.json -f json')
-with open('bandit.json', 'r', encoding="utf-8") as f:
+if res_code < 5:
+    print('Error code')
+
+os.system('bandit -iii -lll -q -r ./skachki/ -o bandit.json -f json')
+with open('bandit.json', 'r', encoding='utf-8') as f:
     res = json.load(f)
 if res['results']:
-    raise RuntimeError('Error security!')
+    raise RuntimeError('Error security')
