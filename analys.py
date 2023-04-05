e3edf9923743841bbbf10dd3de8e03d5a250bed9
Ilya Krisanov <ilya.crisanov@yandex.ru>
2023-04-06 00:44:20 +0300

    Changes

--- analys.py
+++ analys.py
@@ -3,9 +3,9 @@ import json
 pylint = os.popen('pylint app.py').read()
 res_code = float(pylint.split('\n')[-3].split()[6].split('/')[0])
 
-if res_code < 5:
+if res_code < 8:
     print('Error code')
-
+print('\n\nNew comment!!!\n\n')
 os.system('bandit -iii -lll -q -r ./skachki/ -o bandit.json -f json')
 with open('bandit.json', 'r', encoding='utf-8') as f:
     res = json.load(f)
