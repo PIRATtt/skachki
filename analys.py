854a494cce5b21f496aeb639fc3e2479bbbf7d1f
Second User <ilya.crisanov@yandex.ru>
2023-04-06 02:23:02 +0300

    developer commit

--- analys.py
+++ analys.py
@@ -5,7 +5,7 @@ res_code = float(pylint.split('\n')[-3].split()[6].split('/')[0])
 
 if res_code < 8:
     print('Error code')
-print('\n\nNew comment!!!\n\n')
+print('\n\nNew commit grace to GitFlow!!!\n\n')
 os.system('bandit -iii -lll -q -r ./skachki/ -o bandit.json -f json')
 with open('bandit.json', 'r', encoding='utf-8') as f:
     res = json.load(f)
