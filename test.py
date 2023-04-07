0703ac6d469e22720b56d0410818eeec1922ebdb
Ilya Krisanov <ilya.crisanov@yandex.ru>
2023-04-07 19:55:10 +0300

    append pytest

---
+++ test.py
@@ -0,0 +1,27 @@
+from flask.testing import FlaskClient
+from app import app
+
+def test_jockeys():
+    with app.test_client() as client:
+        response = client.get('/jockeys')
+        assert response.status_code == 200
+
+def test_index():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+
+def test_append():
+    with app.test_client() as client:
+        response = client.get('/append')
+        assert response.status_code == 200
+
+def test_battles():
+    with app.test_client() as client:
+        response = client.get('/battles')
+        assert response.status_code == 200
+
+def test_horses():
+    with app.test_client() as client:
+        response = client.get('/horses')
+        assert response.status_code == 200
